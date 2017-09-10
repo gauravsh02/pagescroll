96eabf9924ae9e3e9a8f2a0cbef4fd8614a1700c
Gaurav Shankar <Gaurav Shankar>
2017-09-11 00:38:18 +0530

    UI changes and delete conformation

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@ var arr=[];
 //On Load function
 $("document").ready(function(){
 $("#getText").on('click',function(){
+  $(".banner").html("Please Wait...");
   getText();
 })
  }
@@ -17,7 +18,9 @@ function dat(data){
   for(var i=0; i<20; i++){
     arr[i]=data[i];
    }
+   $('#main').html("");
    $(".banner").hide();
+  //  $(".banner").html("<h2>Post</h2>");
    display();
 }
 function failAlert(){
@@ -27,7 +30,7 @@ function failAlert(){
 
 function display(){
   for(var i=0; i<arr.length; i++){
-    var ele="<div class='well'><p><strong>"+arr[i].title+"</p></strong>"+"<p>"+arr[i].body+'</p><button type="button" class="btn btn-large btn-danger" id="getText" onclick="del('+i+')">Delete</button></div>';
+    var ele="<div class='well'><div class='row'><div class='col-xs-10'><p><strong>"+arr[i].title+"</strong></p>"+"<p>"+arr[i].body+'</p></div><div class="col-xs-2"><button type="button" class="btn btn-large btn-danger" id="getText1" onclick="del('+i+')">Delete<i class="glyphicon glyphicon-trash"></i></button></div></div></div>';
     $('#main').append(ele);
    } 
 }
@@ -35,6 +38,7 @@ function display(){
 function del(id){
   if(confirm("Do you want to delete ?")){
   arr.splice(id,1);
+  alert("The post has been deleted.");
   $('#main').html("");
   console.log(arr);
   display();
